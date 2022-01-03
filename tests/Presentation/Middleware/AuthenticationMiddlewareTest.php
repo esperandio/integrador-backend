46d749cb70c6de2fe6ce4ff56d5e6551e4737bff
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-03 18:53:38 -0300

    test: implementacao de middleware para validar token da requisicao

---
+++ tests/Presentation/Middleware/AuthenticationMiddlewareTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Presentation\Middleware;
+
+use PHPUnit\Framework\TestCase;
+use Test\Doubles\TokenManager\FakeTokenManager;
+use App\Presentation\Middleware\AuthenticationMiddleware;
+use App\Presentation\Middleware\Exceptions\UnauthorizedException;
+use App\Presentation\Ports\{RequestInput, RequestOutput};
+
+class AuthenticationMiddlewareTest extends TestCase
+{
+    public function testReturnRequestOutputInstaceWhenPerfomedWithSuccess(): void
+    {
+        $authenticationMiddleware = new AuthenticationMiddleware(new FakeTokenManager());
+        $requestOutput = $authenticationMiddleware->handle($this->getDefaultRequestInput());
+
+        $this->assertInstanceOf(RequestOutput::class, $requestOutput);
+        $this->assertEquals(1, $requestOutput->body['id']);
+    }
+
+    public function testThrowsExceptionWhenUseCaseFails(): void
+    {
+        $this->expectException(UnauthorizedException::class);
+
+        $authenticationMiddleware = new AuthenticationMiddleware(new FakeTokenManager());
+        $authenticationMiddleware->handle($this->getDefaultRequestInput(''));
+    }
+
+    private function getDefaultRequestInput(string $authorization = '1SIGNED'): RequestInput
+    {
+        return new RequestInput(
+            body: [
+                'authorization' => $authorization
+            ]
+        );
+    }
+}
